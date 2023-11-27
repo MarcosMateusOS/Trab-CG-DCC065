2b91c96812d92f500e19afd1faf606a93458343c
MarcosMateusOS <marcosmateussantos09@gmail.com>
2023-11-27 20:30:22 -0300

    ajustando branch para versão mobile

--- T1/src/camera.js
+++
@@ -1,9 +0,0 @@
-import * as THREE from "three";
-export default function cameraInit(width, heigth,position) {
-  var camera = new THREE.PerspectiveCamera(45, width / heigth * 2 , 0.1, 1000);
-
-  camera.position.copy(position);
-  camera.lookAt(0, 0, 0);
-  return camera;
-}
- 
